666e088b2f2764cc81b00bbc58fe889e3687b6ed
MarkVanMortem <34596876+magister1101@users.noreply.github.com>
2023-06-30 17:23:10 +0800

    new line

--- html/logout.php
+++ html/logout.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-if(isset($_SESSION['email'])){
+if(isset($_SESSION['email'])){ 
 
-    session_destroy();
+    session_destroy(); //destroys the session.
     echo"<script>location.href='employeeLogin.html'</script>";
 
 }
-else{
+else{ //if not logged in but manages to get to click the logout, it will automatically redirect to login page.
     echo"<script>location.href='employeeLogin.html'</script>";
 }
 
